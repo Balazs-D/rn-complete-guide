c7b98b2b628544a2936c8792fde2176de1b92313
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-06-25 17:06:45 +0200

    basic level to do list, add new item and display whoele array

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // Usual syntax
+    // setCourseGoals([...courseGoals, enteredGoal]);
+
+    // ES6 function syntax - always works!
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.inputField} />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.inputField}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
 
-      <View></View>
+      <View>
+        {courseGoals.map((goal, i) => (
+          <Text key={i}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
